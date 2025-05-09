9469d187804321c57943be01e1af3d54d505e28d
cuishuheng1 <cuishuheng1@tal.com>
2025-05-09 11:18:14 +0800

    feat: 1

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
 
   // 全局启用ValidationPipe
   app.useGlobalPipes(new ValidationPipe());
+
+  // 统一接口路径前缀
+  app.setGlobalPrefix('/api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
