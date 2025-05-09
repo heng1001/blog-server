4e0d1ba1cf9491e7689178e87c635c3fcc53e63c
cuishuheng1 <cuishuheng1@tal.com>
2025-05-09 11:49:24 +0800

    feat: 修改app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MailModule } from './mail/mail.module';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { UserModule } from './user/user.module';
+import * as path from 'path';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/my-blog'),
-    MailModule,
+    // TODO:📝
+    ConfigModule.forRoot(),
     MailerModule.forRootAsync({
-      useFactory: () => ({
-        transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('MAIL_SERVER'), // QQ邮箱SMTP服务器
+          port: configService.get<number>('MAIL_PORT'), // QQ邮箱SMTP端口
+          secure: true, // 使用SSL
+          auth: {
+            user: configService.get<string>('MAIL_USER'), // 邮箱账号
+            pass: configService.get<string>('MAIL_PASS'), // 邮箱密码
+          },
+        },
         defaults: {
           from: '"nest-modules" <modules@nestjs.com>',
         },
         template: {
-          dir: __dirname + '/templates',
+          dir: path.join(process.cwd(), 'templates'),
           adapter: new HandlebarsAdapter(),
           options: {
             strict: true,
           },
         },
       }),
+      inject: [ConfigService],
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/my-blog'),
     UserModule,
   ],
   controllers: [AppController],
