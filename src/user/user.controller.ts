f347be70cf3da51bbe7232c64428f39198ac3648
cuishuheng1 <cuishuheng1@tal.com>
2025-05-09 15:19:12 +0800

    feat: 邮箱注册接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Post, Body } from '@nestjs/common';
+
+// 邮件注册DTO
+class RegisterMailDto {
+  email: string;
+  password: string;
+}
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('registerMail')
+  async registerMail(@Body() registerMailDto: RegisterMailDto) {
+    const user = await this.userService.registerWithEmail(
+      registerMailDto.email,
+      registerMailDto.password,
+    );
+
+    return {
+      code: 200,
+      message: '注册成功，请查收验证邮件',
+      data: {
+        email: user.email,
+      },
+    };
+  }
+}
